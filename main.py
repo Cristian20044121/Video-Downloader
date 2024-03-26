2ddb233e3e8a1b3638fa3e83f0c57d6512b5827c
Cristian Caro <crdcaro01@gmail.com>
2024-03-26 00:14:18 -0500

    code modified

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def index():
 def download():
     if request.method == "POST":
         url = request.form["url"]
-        # Verifica si el usuario está accediendo desde un dispositivo Android
-        if 'android' in request.user_agent.platform:
-            download_dir = '/storage/emulated/0/Download'
+        # Define la ruta de descarga en función del tipo de dispositivo
+        if request.user_agent.platform in ['android', 'iphone']:
+            download_dir = os.path.join(os.path.expanduser('~'), '/storage/emulated/0/Download')
         else:
-            download_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
+            download_dir = os.path.join(os.path.expanduser('~'), '/storage/emulated/0/Download')
         video_success = download_video(url, download_dir)
         audio_success = download_audio(url, download_dir)
         if video_success and audio_success:
